a7dc7558254b41a1813fa93655a318ee969d6f0f
Bennett Smrdel <smrdelb@gmail.com>
2021-11-05 18:25:36 -0500

    can submit inputs and store values, clear submited form

--- script.js
+++ script.js
@@ -8,8 +8,32 @@ let employeeData = [
 
 
 function onReady() {
-
+    $('#form-submit').on('click', submitForm);
 }
 
+//Store each form input then empty the form. 
+function submitForm() {
+    let firstName = $('#first-name-input').val();
+    let lastName = $('#last-name-input').val();
+    let id = $('#id-input').val();
+    let title = $('#title-input').val();
+    let salary = $('#salary-input').val();
+    //check if any fields are empty.
+        //send alert if field is empty.
 
+    // if (firstName === '' || lastName === '' || id === '' || title === '' || salary === '') {
+    //     alert('Fill empty fields!');
+    // } else {
+        
+        //Empty the input values.
+        clearForm();
+    }
+//}
 
+function clearForm() {
+    $('#first-name-input').val('');
+    $('#last-name-input').val('');
+    $('#id-input').val('');
+    $('#title-input').val('');
+    $('#salary-input').val('');
+}
