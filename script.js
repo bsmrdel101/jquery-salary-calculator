599f16de0f28ff3c3b56a98ceb5f0e0b5f60c2a1
Bennett Smrdel <smrdelb@gmail.com>
2021-11-05 16:49:56 -0500

    added employeeData array, monthly total on DOM

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 $(document).ready(onReady);
 
+let employeeData = [
+    {firstName:'Jim', lastName:'Jeff', id:1468, title:'fry cook', salary:90000}
+];
+
+
+
+
 function onReady() {
-    
+
 }
 
 
